25def8d30c3994160a1f3f96c62e1acea97073e6
Marcos Mello <marcosfrm@gmail.com>
2021-06-08 21:37:21 -0300

    Suporte a instalações UEFI
    \o/

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-Shell script que uso no SystemRescueCD para restaurar imagem do FSArchiver, contendo uma única partição (nada de swap) e apagando todos os dados do disco de destino.
+Shell script que uso no SystemRescue para restaurar imagem do FSArchiver apagando **todos** os dados do disco de destino.
 
-Não suporta UEFI.
+Para usá-lo, renomeie o script para `autorun` e copie-o para a raiz do pendrive onde está o SystemRescue (6.1.7 ou superior), junto com a imagem do FSArchiver. **Não** use a opção de boot `copytoram`.
 
-Para usá-lo, renomeie o script para `autorun` e copie-o para a raiz do pendrive onde está o SystemRescueCD (6.1.7 ou superior), junto com a imagem do FSArchiver. **Não** use a opção de boot `copytoram`.
+- Para instalações BIOS/CSM, a imagem precisa ter apenas um sistema de arquivos (id 0) da partição raiz.
+  - Exemplo de captura: `fsarchiver savefs linux.fsa /dev/sda1` (`/dev/sda1` é partição raiz).
+
+- Para instalações UEFI, a imagem dever ter dois sistema de arquivos, sendo o primeiro (id 0) da partição raiz e o segundo (id 1) da partição EFI.
+  - Exemplo de captura: `fsarchiver savefs linux.fsa /dev/sda2 /dev/sda1` (`/dev/sda2` é partição raiz e `/dev/sda1` é a partição EFI).
+
+Qualquer outra partição não é suportada, incluindo swap.
+
+O script não faz diferenciação pelo modo em que o SystemRescue foi inicializado (BIOS/CSM ou UEFI). No caso de instalações UEFI, será possível restaurá-las mesmo inicializando com BIOS/CSM, pois não mexe nas variáveis do firmware.
--- fsarchiver_linux.sh
+++ fsarchiver_linux.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 # Marcos FRM
-# 25/07/2020
+# 08/06/2021
 
 # Nome do arquivo com a imagem do FSArchiver, presente na raiz do pendrive
 ARQUIVOFSA="linux.fsa"
 
+# Ponto de montagam da partição EFI no sistema restaurado
+ESPMNT='/boot/efi'
+
 # --------------------------------------
 
-unset MUDAUUID
-unset MUDAFS
-unset LISTAFS
-unset FSNOVO
-unset FSAOPT
+declare -A IMGINFO FSORIG FSNOVO UUIDORIG UUIDNOVO PART
 PASSADAS=0
 
 PENMNT="/run/archiso/bootmnt"
@@ -20,7 +19,6 @@ DESTMNT="/mnt/dest-$RANDOM"
 
 mostraerro() {
     rmdir $DESTMNT 2>/dev/null
-
     echo      >&2
     echo "$1" >&2
     echo      >&2
@@ -55,12 +53,21 @@ mountpoint -q $PENMNT          || mostraerro "Pendrive não encontrado. Não use
 echo -e "\nPendrive:\t$PENMNT\nDestino:\t$DESTMNT\n"
 
 if FSAINFO=$(LANG=C fsarchiver archinfo "$PENMNT/$ARQUIVOFSA" 2>&1); then
-    FSORIG=$(awk -F':[[:blank:]]*' '/^Filesystem format/ {print $2}' <<< "$FSAINFO")
-    if (( $(awk -F':[[:blank:]]*' '/^Filesystems count/ {print $2}' <<< "$FSAINFO") > 1 )); then
-        mostraerro "Imagem contém mais de um sistema de arquivos. Cancelado."
-    elif [[ ! $FSORIG =~ ext[2-4]|xfs|btrfs|jfs|reiserfs ]]; then
-        mostraerro "Imagem não contém um sistema de arquivos suportado (EXT2/3/4, XFS, Btrfs, JFS ou ReiserFS)."
+    (( $(awk -F ':[[:blank:]]*' '/^Filesystems count/ {print $2}' <<< "$FSAINFO") > 2 )) && \
+        echo -e "\nMais de dois sistemas de arquivos na imagem. Ignorando do terceiro em diante.\n"
+    # ID 0 -> raiz
+    # ID 1 -> esp
+    IMGINFO[raiz]=$(awk '/Filesystem id in archive:[[:blank:]]*0/,/^$/' <<< "$FSAINFO")
+    if IMGINFO[esp]=$(awk '/Filesystem id in archive:[[:blank:]]*1/,/^$/ {e=1; print}; END{exit !e}' <<< "$FSAINFO"); then
+        FSORIG[esp]=$(awk -F ':[[:blank:]]*' '/^Filesystem format/ {print $2}' <<< "${IMGINFO[esp]}")
+        UUIDORIG[esp]=$(awk -F ':[[:blank:]]*' '/^Filesystem uuid/ {print $2}' <<< "${IMGINFO[esp]}")
+        [[ ${FSORIG[esp]} == vfat ]] || \
+            mostraerro "Imagem da partição EFI não possui sistema de arquivos FAT."
     fi
+    FSORIG[raiz]=$(awk -F ':[[:blank:]]*' '/^Filesystem format/ {print $2}' <<< "${IMGINFO[raiz]}")
+    UUIDORIG[raiz]=$(awk -F ':[[:blank:]]*' '/^Filesystem uuid/ {print $2}' <<< "${IMGINFO[raiz]}")
+    [[ ${FSORIG[raiz]} =~ ext[2-4]|xfs|btrfs|jfs|reiserfs ]] || \
+        mostraerro "Imagem não contém um sistema de arquivos suportado (EXT2/3/4, XFS, Btrfs, JFS ou ReiserFS)."
 else
     mostraerro "Arquivo não é uma imgem do FSArchiver."
 fi
@@ -70,7 +77,7 @@ PENDEV=$(findmnt -rno SOURCE -M $PENMNT)
 # util-linux >= 2.22
 for DISCO in $(lsblk -I 8 -drno NAME); do
     [[ $PENDEV =~ $DISCO ]] && continue
-    MODELO=$(cat /sys/block/$DISCO/device/model)
+    MODELO=$(< /sys/block/$DISCO/device/model)
     TAMANHO=$(calctam $(blockdev --getsize64 /dev/$DISCO))
     echo "/dev/$DISCO $TAMANHO - $MODELO"
     (( $PASSADAS == 0 )) && PRIMEIRODISCO=$DISCO
@@ -104,7 +111,7 @@ while true; do
 done
 
 echo
-echo "Mudar sistema de arquivos? O atual é \"$FSORIG\"."
+echo "Mudar sistema de arquivos? O atual é \"${FSORIG[raiz]}\"."
 select RESP in SIM NÃO; do
     case $RESP in
         SIM)
@@ -119,7 +126,7 @@ done
 
 if [[ $MUDAFS ]]; then
     for FSATUAL in ext2 ext3 ext4 xfs btrfs jfs reiserfs; do
-        [[ $FSATUAL == $FSORIG ]] && continue
+        [[ $FSATUAL == ${FSORIG[raiz]} ]] && continue
         LISTAFS+="$FSATUAL "
     done
     echo
@@ -127,31 +134,31 @@ if [[ $MUDAFS ]]; then
     select RESP in $LISTAFS; do
         case $RESP in
             ext2)
-                FSNOVO=ext2
+                FSNOVO[raiz]=ext2
                 break
             ;;
             ext3)
-                FSNOVO=ext3
+                FSNOVO[raiz]=ext3
                 break
             ;;
             ext4)
-                FSNOVO=ext4
+                FSNOVO[raiz]=ext4
                 break
             ;;
             xfs)
-                FSNOVO=xfs
+                FSNOVO[raiz]=xfs
                 break
             ;;
             btrfs)
-                FSNOVO=btrfs
+                FSNOVO[raiz]=btrfs
                 break
             ;;
             jfs)
-                FSNOVO=jfs
+                FSNOVO[raiz]=jfs
                 break
             ;;
             reiserfs)
-                FSNOVO=reiserfs
+                FSNOVO[raiz]=reiserfs
                 break
             ;;
         esac
@@ -188,30 +195,53 @@ done
 echo
 echo "Particionando..."
 # util-linux >= 2.36
-# https://github.com/karelzak/util-linux/commit/c3ef1268a0b4b0d01dd76043a865b4acf144f3c9
-echo ',,L,*' | sfdisk --quiet --lock=yes --wipe=always --wipe-partitions=always --label=dos $DEV
+if [[ ${IMGINFO[esp]} ]]; then
+    # ESP de 500 MiB
+    echo -e ',500M,U\n,,L' | sfdisk --quiet --lock=yes --wipe=always --wipe-partitions=always --label=gpt $DEV
+    PART[esp]=${DEV}1
+    PART[raiz]=${DEV}2
+else
+    echo ',,L,*' | sfdisk --quiet --lock=yes --wipe=always --wipe-partitions=always --label=dos $DEV
+    PART[raiz]=${DEV}1
+fi
 udevadm settle
 
 echo
 echo "Restaurando..."
-UUIDORIG=$(awk -F':[[:blank:]]*' '/^Filesystem uuid/ {print $2}' <<< "$FSAINFO")
-[[ $FSNOVO ]] && FSAOPT=",mkfs=$FSNOVO"
+[[ ${FSNOVO[raiz]} ]] && FSAOPT=",mkfs=${FSNOVO[raiz]}"
 if [[ $MUDAUUID ]]; then
-    UUIDNOVO=$(uuidgen)
-    FSAOPT+=",uuid=$UUIDNOVO"
+    UUIDNOVO[raiz]=$(uuidgen)
+    FSAOPT+=",uuid=${UUIDNOVO[raiz]}"
 fi
 # fsarchiver >= 0.8.0
-fsarchiver -j $(nproc) restfs "$PENMNT/$ARQUIVOFSA" id=0,dest=${DEV}1$FSAOPT || \
-    mostraerro "Falha ao restaurar imagem."
+fsarchiver -j $(nproc) restfs "$PENMNT/$ARQUIVOFSA" id=0,dest=${PART[raiz]}${FSAOPT} || \
+    mostraerro "Falha ao restaurar imagem (raiz)."
+
+FSAOPT=
+if [[ ${IMGINFO[esp]} ]]; then
+    if [[ $MUDAUUID ]]; then
+        UUIDNOVO[esp]=$(openssl rand -hex 4 | tr '[:lower:]' '[:upper:]')
+        # https://github.com/fdupoux/fsarchiver/pull/106
+        #FSAOPT=",uuid=${UUIDNOVO[esp]}"
+        FSAOPT=",mkfsopt=-i${UUIDNOVO[esp]}"
+    fi
+    fsarchiver -j $(nproc) restfs "$PENMNT/$ARQUIVOFSA" id=1,dest=${PART[esp]}${FSAOPT} || \
+        mostraerro "Falha ao restaurar imagem (esp)."
+fi
 
 echo
 echo "Definindo configurações..."
 DESTMNTOPTS='-o X-mount.mkdir'
 # ReiserFS precisa de opções de montagem para habilitar ACLs e XATTRs
-if [[ ( ! $FSNOVO && $FSORIG == reiserfs ) || $FSNOVO == reiserfs ]]; then
+if [[ ( ! ${FSNOVO[raiz]} && ${FSORIG[raiz]} == reiserfs ) || ${FSNOVO[raiz]} == reiserfs ]]; then
     DESTMNTOPTS+=',acl,user_xattr'
 fi
-mount ${DEV}1 $DESTMNT $DESTMNTOPTS 2>/dev/null || mostraerro "Falha ao montar destino."
+mount ${PART[raiz]} $DESTMNT $DESTMNTOPTS 2>/dev/null || mostraerro "Falha ao montar destino (raiz)."
+
+if [[ ${IMGINFO[esp]} ]]; then
+    mount ${PART[esp]} ${DESTMNT}${ESPMNT} -o 'utf8,iocharset=ascii' 2>/dev/null || \
+        mostraerro "Falha ao montar destino (esp)."
+fi
 mount --bind /dev $DESTMNT/dev
 mount --bind /proc $DESTMNT/proc
 mount --bind /sys $DESTMNT/sys
@@ -225,21 +255,28 @@ rm -f $DESTMNT/etc/udev/rules.d/70-persistent-net.rules
 rm -f $DESTMNT/etc/NetworkManager/system-connections/*
 sed -i '/^no-auto-default=/d' $DESTMNT/etc/NetworkManager/NetworkManager.conf 2>/dev/null
 
-if [[ $FSNOVO ]]; then
-    sed -i "/$UUIDORIG/ s/$FSORIG/$FSNOVO/" $DESTMNT/etc/fstab
+if [[ ${FSNOVO[raiz]} ]]; then
+    sed -i "/${UUIDORIG[raiz]}/ s/${FSORIG[raiz]}/${FSNOVO[raiz]}/" $DESTMNT/etc/fstab
     # ReiserFS: "acl,user_xattr", "user_xattr,acl", "acl", "user_xattr"
     # outros sistemas: "defaults"
-    if [[ $FSORIG == reiserfs ]]; then
-        sed -ri "/$UUIDORIG/ s/[[:blank:]]+(acl(,user_xattr)?|user_xattr(,acl)?)[[:blank:]]+/\tdefaults\t/" \
+    if [[ ${FSORIG[raiz]} == reiserfs ]]; then
+        sed -ri "/${UUIDORIG[raiz]}/ s/[[:blank:]]+(acl(,user_xattr)?|user_xattr(,acl)?)[[:blank:]]+/\tdefaults\t/" \
             $DESTMNT/etc/fstab
-    elif [[ $FSNOVO == reiserfs ]]; then
-        sed -ri "/$UUIDORIG/ s/[[:blank:]]+defaults[[:blank:]]+/\tacl,user_xattr\t/" \
+    elif [[ ${FSNOVO[raiz]} == reiserfs ]]; then
+        sed -ri "/${UUIDORIG[raiz]}/ s/[[:blank:]]+defaults[[:blank:]]+/\tacl,user_xattr\t/" \
             $DESTMNT/etc/fstab
     fi
 fi
 
 if [[ $MUDAUUID ]]; then
-    sed -i "s/$UUIDORIG/$UUIDNOVO/" $DESTMNT/etc/fstab
+    if [[ ${IMGINFO[esp]} ]]; then
+        # 12345678 -> 1234-5678
+        UUIDORIG[esp]=$(printf '%s-%s' ${UUIDORIG[esp]:0:4} ${UUIDORIG[esp]:4:4})
+        UUIDNOVO[esp]=$(printf '%s-%s' ${UUIDNOVO[esp]:0:4} ${UUIDNOVO[esp]:4:4})
+        sed -i "s/${UUIDORIG[esp]}/${UUIDNOVO[esp]}/" $DESTMNT/etc/fstab
+    fi
+    sed -i "s/${UUIDORIG[raiz]}/${UUIDNOVO[raiz]}/" $DESTMNT/etc/fstab
+
     [[ -e $DESTMNT/var/lib/dbus/machine-id && ! -L $DESTMNT/var/lib/dbus/machine-id ]] && \
         dbus-uuidgen > $DESTMNT/var/lib/dbus/machine-id
     > $DESTMNT/etc/machine-id
@@ -259,7 +296,7 @@ case $ID in
             [[ ${RDLISTA##*/} =~ rescue ]] && continue
             KVER=$(sed 's/^initramfs-//;s/\.img$//' <<< ${RDLISTA##*/})
             [[ -d $DESTMNT/usr/lib/modules/${KVER:-xyz} ]] && \
-                chroot $DESTMNT dracut --verbose --force /boot/${RDLISTA##*/} $KVER
+                chroot $DESTMNT dracut --force /boot/${RDLISTA##*/} $KVER
         done
     ;;
     opensuse)
@@ -276,14 +313,16 @@ case $ID in
     ;;
 esac
 
-echo
-echo "Instalando GRUB..."
-if [[ $ID == debian || $ID == ubuntu || $ID == arch ]]; then
-    [[ $MUDAUUID ]] && chroot $DESTMNT grub-mkconfig -o /boot/grub/grub.cfg
-    chroot $DESTMNT grub-install --recheck --no-floppy $DEV
-else
-    [[ $MUDAUUID ]] && chroot $DESTMNT grub2-mkconfig -o /boot/grub2/grub.cfg
-    chroot $DESTMNT grub2-install --recheck --no-floppy $DEV
+if [[ ! ${IMGINFO[esp]} ]]; then
+    echo
+    echo "Instalando GRUB..."
+    if [[ $ID == debian || $ID == ubuntu || $ID == arch ]]; then
+        [[ $MUDAUUID ]] && chroot $DESTMNT grub-mkconfig -o /boot/grub/grub.cfg
+        chroot $DESTMNT grub-install --recheck --no-floppy $DEV
+    else
+        [[ $MUDAUUID ]] && chroot $DESTMNT grub2-mkconfig -o /boot/grub2/grub.cfg
+        chroot $DESTMNT grub2-install --recheck --no-floppy $DEV
+    fi
 fi
 
 echo
